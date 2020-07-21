11345f5a1f7aa9cb1e3718efe88623e5d33fabc9
AGopalakrishna <AGopalakrishna@host.docker.internal>
2020-07-21 12:46:03 +0530

    First commit

---
+++ AzureDemoPipeline/src/main/java/com/opteamix/AzureDemoPipeline/App.java
@@ -0,0 +1,13 @@
+package com.opteamix.AzureDemoPipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Akg!" );
+    }
+}
